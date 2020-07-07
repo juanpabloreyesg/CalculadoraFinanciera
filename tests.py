013c38438ea01371e45374f78b198dca3386b8ef
Daraexus <juanpabloreyesg@gmail.com>
2020-07-07 18:32:03 -0500

    testing and testing

---
+++ tests.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import unittest
+import calculadora_riesgo_financiero as crf
+
+
+class AllTesting(unittest.TestCase):
+    
+    def setUp(self):
+        self.perfil_base = {"Nombre":"Alan", "Apellido":"Smithee", "Edad":10, "Salario Mensual":100, "Deuda":50, "Riesgo":"NA"}
+        
+    def test_creation(self):
+        self.assertEqual(crf.crear_perfil("Alan", "Smithee",10,100,50), self.perfil_base)
+
+    def test_failure(self):
+        self.assertEqual(1,2)
+
+        
+if __name__ == '__main__':
+    unittest.main()
